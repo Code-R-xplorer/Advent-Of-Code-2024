972b9c97b17828de4335623a62a1fd7b297b1a7e
TheAmazingHazza <contact@harryvallance.com>
2024-12-01 07:19:21 +0000

    Day 1 Part 1 Moved utils to root folder

--- src/Day_01.py
+++ src/Day_01.py
@@ -1,4 +1,30 @@
 from utils import read_file
 
-values = read_file(1, str, True)
 
+def transform_to_two_lists(line: str):
+    # Split the line by whitespace and convert each part to an integer
+    return tuple(map(int, line.split()))
+
+
+values = read_file(1, transform_to_two_lists, False)
+
+# Use zip to unpack the tuples into two separate lists
+list1, list2 = zip(*values)
+
+# Convert back into lists for further use
+list1 = list(list1)
+list2 = list(list2)
+
+# Sort both lists into ascending order
+sorted_list_1 = sorted(list1)
+sorted_list_2 = sorted(list2)
+
+
+# Calculate the total distance by comparing the distance with each pair in the lists
+total_distance = 0
+
+for i in range(len(sorted_list_1)):
+    total_distance += abs(sorted_list_1[i] - sorted_list_2[i])
+
+print(total_distance)
+# Part 1: 1320851
