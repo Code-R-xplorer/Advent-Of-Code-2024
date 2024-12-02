b796cef3894a1354133b81a05f6c51cd85ff97d5
TheAmazingHazza <contact@harryvallance.com>
2024-12-02 07:32:24 +0000

    Day 2 Part 1

--- src/Day_02.py
+++ src/Day_02.py
@@ -1,4 +1,34 @@
 from utils import read_file
 
-values = read_file(2, str, True)
 
+def transform(line):
+    return [int(c) for c in line.split()]
+
+
+values = read_file(2, transform, False)
+
+safe_reports = 0
+
+for report in values:
+    increasing = report[0] > report[1]
+    safe_reports += 1
+
+    for i in range(len(report) - 1):
+        if report[i] == report[i + 1]:
+            safe_reports -= 1
+            break
+        difference = abs(report[i] - report[i + 1])
+        if difference > 3:
+            safe_reports -= 1
+            break
+        if increasing:
+            if report[i] < report[i + 1]:
+                safe_reports -= 1
+                break
+        else:
+            if report[i] > report[i + 1]:
+                safe_reports -= 1
+                break
+
+print(safe_reports)
+# Part 1: 639
