9c7f5d0c2b81f14790f1ccdc0833c84aa12865ff
TheAmazingHazza <contact@harryvallance.com>
2024-12-03 12:13:43 +0000

    Day 3 Part 2

--- src/Day_03.py
+++ src/Day_03.py
@@ -1,7 +1,7 @@
 from utils import read_file
 import re
 
-values = read_file(3, str, True)
+values = read_file(3, str, False)
 
 # Regular expression to match valid mul(X,Y)
 pattern = r"mul\(\d{1,3},\d{1,3}\)"
@@ -27,3 +27,36 @@ for x, y in results:
 
 print(f'Part 1: {total_results}')
 # Part 1: 173419328
+
+pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)"
+
+matches = []
+
+for line in values:
+    matches += re.findall(pattern, line)
+
+instructions = []
+do_instruction = True
+
+for i in range(len(matches)):
+    if matches[i] == "don't()":
+        do_instruction = False
+        continue
+    if matches[i] == "do()":
+        do_instruction = True
+        continue
+    if do_instruction:
+        instructions.append(matches[i])
+
+# Extract X and Y for each match
+results = [re.match(extract_pattern, match).groups() for match in instructions]
+
+# Convert the tuples to integers (optional)
+results = [(int(x), int(y)) for x, y in results]
+
+total_results = 0
+
+for x, y in results:
+    total_results += (x * y)
+
+print(f'Part 2: {total_results}')
