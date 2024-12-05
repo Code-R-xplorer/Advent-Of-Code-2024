f51161bfa38064d91a0a6caf3a01c948d39f7ee4
TheAmazingHazza <contact@harryvallance.com>
2024-12-05 11:18:10 +0000

    Day 5 Part 2

--- src/Day_05.py
+++ src/Day_05.py
@@ -23,8 +23,8 @@ for rule in result[0]:
 
 updates = []
 
-for update in result[1]:
-    updates.append(list(map(int, update.split(','))))
+for r in result[1]:
+    updates.append(list(map(int, r.split(','))))
 
 
 def check_update_order(update, rules):
@@ -43,4 +43,43 @@ for u in updates:
         total_correct_middle += u[int((len(u) - 1) / 2)]
 
 print(f'Part 1: {total_correct_middle}')
+
+
 # Part 1: 5329
+
+def reorder_update(update, rules):
+    """Reorder an update to adhere to the rules."""
+    # Create a dependency graph
+    dependencies = {x: set() for x in update}
+    for x, y in rules:
+        if x in update and y in update:
+            dependencies[y].add(x)
+
+    # Perform a topological sort
+    sorted_update = []
+    while dependencies:
+        # Find items with no dependencies
+        no_deps = [item for item, deps in dependencies.items() if not deps]
+        if not no_deps:
+            raise ValueError("Cycle detected in rules")
+
+        # Add items with no dependencies to the sorted list
+        for item in no_deps:
+            sorted_update.append(item)
+            del dependencies[item]
+
+        # Remove the items from other dependencies
+        for deps in dependencies.values():
+            deps.difference_update(no_deps)
+
+    return sorted_update
+
+
+total_reordered_middle = 0
+
+for u in updates:
+    if not check_update_order(u, page_rules):
+        total_reordered_middle += reorder_update(u, page_rules)[int((len(u) - 1) / 2)]
+
+print(f'Part 2: {total_reordered_middle}')
+# Part 2: 5833
